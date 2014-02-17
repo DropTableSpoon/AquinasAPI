8b89f1fd9ca98a9e3306fb90299ea2a538d42281
Elite6809 <tgalvin97@gmail.com>
2014-02-17 20:08:59 +0000

    Added an extension method to safely call extensions
    This is more for convenience than anything.

---
+++ EventHandlerExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Aquinas
+{
+    /// <summary>
+    /// A static container class for extension methods for the EventHandler&lt;T&gt; class.
+    /// </summary>
+    public static class EventHandlerExtensions
+    {
+        /// <summary>
+        /// Raises the event in a thread-safe manner.
+        /// </summary>
+        /// <param name="sender">The object raising the event.</param>
+        /// <param name="argument">The argument to send to the event handler.</param>
+        public static void Raise<T>(this EventHandler<T> theEvent, object sender, T argument) where T : EventArgs
+        {
+            if (theEvent != null)
+            {
+                theEvent(sender, argument);
+            }
+        }
+    }
+}
